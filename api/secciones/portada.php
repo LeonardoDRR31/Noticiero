d0fcfa29b767e4c93b79bd45362619bf9d2376de
Leonardo <david_31_leo@hotmail.com>
2025-11-04 16:48:00 -0500

    primer

--- api/secciones/portada.php
+++ api/secciones/portada.php
@@ -2,14 +2,28 @@
 /*****
 SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
-
 $portada = [
-"titulo" => "ROJAS ROJAS LEONARDO DAVID",
-"autor" => "jperez",
-"resumen" => "Las noticias mas importantes del 2025",
-"img"=>"../img/portada.jpg"
+    "titulo" => "ROJAS ROJAS LEONARDO DAVID",
+    "autor" => "jperez",
+    "resumen" => "Las noticias más importantes del 2025",
+    "img" => "../img/portada.jpg"
 ];
 ?>
 
-<!-- Aquí se muestra la imagen -->
-<img src="<?php echo $portada["img"]; ?>" alt="Imagen de la noticia" width="400">
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title><?php echo $portada["titulo"]; ?></title>
+</head>
+<body>
+
+    <h1><?php echo $portada["titulo"]; ?></h1>
+    <p><strong>Autor:</strong> <?php echo $portada["autor"]; ?></p>
+    <p><?php echo $portada["resumen"]; ?></p>
+
+    <!-- Aquí se muestra la imagen -->
+    <img src="<?php echo $portada["img"]; ?>" alt="Imagen de portada" width="300">
+
+</body>
+</html>
