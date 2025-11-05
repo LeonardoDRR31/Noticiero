4abe5880d56d2036cb586ab74ac768889495b4bc
Leonardo <david_31_leo@hotmail.com>
2025-11-05 03:17:15 -0500

    Update index.php

--- api/index.php
+++ api/index.php
@@ -35,7 +35,7 @@
     ?>
     <div class="jumbotron p-3 p-md-5 text-white rounded bg-dark">
       <div class="col-md-12 px-0">
-        <img class="card-img-left flex-auto d-none d-md-block" src="api/img/portada.jpg"
+        <img class="card-img-left flex-auto d-none d-md-block" src="/api/img/portada.jpg"
             alt="Imagen internacional" style="width: 200px; height: auto; object-fit: cover;">
         <h1 class="display-4 font-italic">
           <?php
