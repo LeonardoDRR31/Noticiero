4df8d12aa07771f9b88d37acff5fe03729d8f93a
Leonardo <david_31_leo@hotmail.com>
2025-11-04 22:21:57 -0500

    1

--- api/index.php
+++ api/index.php
@@ -80,9 +80,7 @@
       <!-- Nacional -->
       <div class="col-md-6">
         <div class="card flex-md-row mb-4 shadow-sm">
-          <img src="https://static.wikia.nocookie.net/disney/images/7/7c/Cass-Hamada.png/revision/latest?cb=20171018011928&path-prefix=es" alt="soto">
-          <img class="card-img-left flex-auto d-none d-md-block" src="<?php echo $nacional['img']; ?>"
-            alt="Imagen nacional" style="width: 200px; height: auto; object-fit: cover;">
+          <img src="/api/img/canciller.jpg" alt="soto">
           <div class="card-body d-flex flex-column align-items-start col-md-12">
             <strong class="d-inline-block mb-2 text-success">Nacional</strong>
             <h3 class="mb-0">
