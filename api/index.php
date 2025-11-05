fbaa15c37341c8be2240245aa43975b785caad5b
Leonardo <david_31_leo@hotmail.com>
2025-11-05 16:07:01 -0500

    Update index.php

--- api/index.php
+++ api/index.php
@@ -33,7 +33,8 @@
     include("secciones/portada.php");
     ?>
     <div class="jumbotron p-3 p-md-5 text-white rounded bg-dark">
-      <div class="col-md-12 px-0"; style="background-image: url('/img/portada.jpg'); bgackground-size: cover; background-repeat: no-repeat;">
+      <div class="col-md-12 px-0">
+        <div class="col-12 text-center"; style="background-image: url('/img/portada.jpg'); bgackground-size: cover; background-repeat: no-repeat;">
         <div class="col-12 text-center">
         <h1 class="display-4 font-italic">
           <?php
